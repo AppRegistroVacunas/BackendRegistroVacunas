0481f5736fac4d221f09241f16aad78b28e3f686
Anthony Javier <90065903+Anthony-Bueno@users.noreply.github.com>
2023-06-06 11:56:40 -0500

    feat: add main project

---
+++ src/main/java/com/AppRegistroVacunas/demo/AppVacunasApplication.java
@@ -0,0 +1,13 @@
+package com.AppRegistroVacunas.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AppVacunasApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AppVacunasApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/AppRegistroVacunas/demo/AppVacunasApplicationTests.java
@@ -0,0 +1,13 @@
+package com.AppRegistroVacunas.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AppVacunasApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
