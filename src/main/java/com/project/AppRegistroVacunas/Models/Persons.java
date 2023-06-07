e127422b43cf10eef7c322ef07efa47f8a1ef5f2
Alexander <alshmex209@gmail.com>
2023-06-06 21:13:02 -0500

    feat: add columms in our tables

---
+++ src/main/java/com/project/AppRegistroVacunas/Models/Persons.java
@@ -0,0 +1,17 @@
+package com.project.AppRegistroVacunas.Models;
+
+import java.util.Date;
+
+public class Persons {
+
+    private int id;
+    private Long dni;
+    private String name;
+    private String lastname;
+    private String email;
+    private String job;
+    private Date birthday;
+    private char sex;
+
+
+}
---
+++ src/main/java/com/project/AppRegistroVacunas/Models/Vaccines.java
@@ -0,0 +1,10 @@
+package com.project.AppRegistroVacunas.Models;
+
+public class Vaccines {
+
+    private String id;
+    private String name;
+    private String madeIn;
+    private String brand;
+    private String
+}
